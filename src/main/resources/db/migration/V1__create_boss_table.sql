c79179b556295a4ecdfc63f995ddb9b720366d41
Tairrque Baker <tairrque.r.baker.mil@army.mil>
2026-04-18 10:04:46 -0500

    react + tailwind setup complete

---
+++ src/main/resources/db/migration/V1__create_boss_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE boss (
+                      id BIGSERIAL PRIMARY KEY,
+                      name VARCHAR(255) NOT NULL,
+                      company VARCHAR(255) NOT NULL,
+                      department VARCHAR(255)
+);
