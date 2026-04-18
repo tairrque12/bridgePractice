23becabae2404bdd1f194020cfc2fb6a210b254d
Tairrque Baker <tairrque.r.baker.mil@army.mil>
2026-04-18 08:48:59 -0500

    initial project setup

---
+++ src/main/java/com/ratemyboss/ratemyboss2/Ratemyboss2Application.java
@@ -0,0 +1,13 @@
+package com.ratemyboss.ratemyboss2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ratemyboss2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ratemyboss2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ratemyboss/ratemyboss2/Ratemyboss2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratemyboss.ratemyboss2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ratemyboss2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
